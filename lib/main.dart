1f3fc2ae7a83834a0f88d2cd824587ba103d48c6
im-minion <vaibhavminiyar@gmail.com>
2018-03-03 00:33:11 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import './pages/landing_page.dart';
-import './pages/quiz_page.dart';
-import './pages/score_page.dart';
 void main(){
   runApp(new MaterialApp(
     home: new LandingPage(),
